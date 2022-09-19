d8d6dfb24a7927cd6b08bd435ee1dccdc2c50210
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2022-09-19 19:40:33 +0200

    Added images.

--- header-home.php
+++ header-home.php
@@ -9,5 +9,6 @@
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet">
     <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     
 </head>
--- index.php
+++ index.php
@@ -1,8 +1,114 @@
 <?php get_template_part('header-home'); ?>
 
 <body>
+<div class="navbar" id="navbar">
+      <div class="navbar-brand">
+        <a href="/">
+          <img src="./assets/logo.svg" alt="Brand Logo" id="brandLogo" />
+        </a>
+      </div>
+      <div class="navbar-toggler"></div>
+      <div class="collapse" >
+        <div class="me-auto nav" id="nav" navbar>
+          <div class="nav-item">
+            <div class="nav-link">
+              <a href="/" id="buttonLink">Start</a>
+            </div>
+          </div>
+          <div class="nav-item">
+           <div class="nav-link">
+              <a href="/AboutView" id="buttonLink">O nas</a>
+           </div>
+          </div>
+          <div class="nav-item">
+           <div class="nav-link">
+              <a href="/ContactView" id="buttonLink">Kontakt</a>
+            </div>
+          </div>
+
+          <ul class="uncontrolled-dropdown nav inNavbar" >
+            <div class="dropdown-toggle nav" id="buttonLinkMore">
+              Obiekty           
+            </div>
+            <ul class="dropdownMenu down">
+              <ul class="uncontrolled-dropdown">
+                <button class="dropdown-toggle nav" id="buttonLinkMore">Hydrofornie</button>
+                <ul class="dropdownMenu down">
+                  <li class="dropdown-item">
+                    <a href="/HydroLwView" id="buttonLinkMoreItem">Lidzbark Warmiński</a>
+                  </li>
+                  <li class="dropdown-item">
+                    <a href="/HydroGiView" id="buttonLinkMoreItem">Górowo Iławeckie</a>
+                  </li >
+                  <li class="dropdown-item">
+                    <a href="/HydroKolView" id="buttonLinkMoreItem">Kolno</a>
+                  </li>
+                  <li class="dropdown-item">
+                    <a href="/HydroLubView" id="buttonLinkMoreItem">Lubomino</a>
+                  </li >
+                </li>
+              </ul>
+
+              <ul class="uncontrolled-dropdown">
+                <button class="dropdown-toggle nav" id="buttonLinkMore">
+                  Oczyszczalnie ścieków
+                  <ArrowDropDown
+                    id="iconDropDown"
+                    style="margin-left: -7px"
+                  />
+                </button >
+                <ul class="dropdown-menu down">
+                  <li class="dropdown-item">
+                    <a href="/OczyszczLwView" id="buttonLinkMoreItem">
+                      Lidzbark Warmiński
+                    </a>
+                  </li >
+                  <li class="dropdown-item">
+                    <a href="/OczyszczGiView" id="buttonLinkMoreItem">
+                      Górowo Iławeckie
+                    </a>
+                  </li>
+                  <li class="dropdown-item">
+                    <a href="/OczyszczKolView" id="buttonLinkMoreItem">
+                      Kolno
+                    </a>
+                  </li >
+                  <li class="dropdown-item">
+                    <a href="/OczyszczLubView" id="buttonLinkMoreItem">
+                      Lubomino
+                    </a>
+                  </li>
+                </ul>
+              </ul>
+            </ul>
+          </ul>
+</div>
+        <div>
+          <button type="submit" id="accessibleIconButton"></button>
+          <button type="submit" id="accessibleIconButton"></button>
+          <button type="submit" id="accessibleIconButton"></button>
+          <button type="submit" id="accessibleIconButton"></button>
+        </div>
+        <div>
+          <a
+            href="http://lidzbarkwarminski-ekowod.bip-wm.pl"
+            target="_blank"
+            rel="noopener noreferrer"
+            id="linkFooter">
+            <img
+              src="./assets/bip.png"
+              alt="bip logo"
+              style="height: 30px"
+              title="Biuletyn Informacji Publicznej"
+            />
+          </a>
+        </div>
+      </div>
+</div>
     <main>
-   
+    
+
+
     </main>
 
     <?php get_footer(); ?>
